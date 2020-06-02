359849c073b01f71218cba99be83d08e4fea3afa
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-02 17:07:02 +0900

    debug2

---
+++ db/migrate/20200602075624_rename_content_column_to_blogs.rb
@@ -0,0 +1,9 @@
+class RenameContentColumnToBlogs < ActiveRecord::Migration[5.2]
+  def up
+    change_column :blogs, :content, :string
+  end
+
+  def down
+    change_column :blogs, :content, :integer
+  end
+end
